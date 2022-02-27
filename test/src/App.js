2a35e89c3d0d3508aba62be6c02567ca509659bf
Subhabrata <subhabrata@Subhabratas-MacBook-Air.local>
2022-02-27 20:30:39 +0530

    react starter project

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import HelloWorld from './components/HelloWorld'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
